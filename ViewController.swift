ae9c4f02d08f65d2917af62583ea99603b4e6077
humeyrasaym <106193215+humeyrasaym@users.noreply.github.com>
2024-05-03 23:50:42 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let eggTimes = ["Soft" : 5, "Medium": 7, "Hard": 12]
+    
+
+    @IBAction func hardnessPressed(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        print(hardness, eggTimes[hardness]!)
+        
+    }
+    
+}
